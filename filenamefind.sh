33422ca8a5ec7132eb09233aacc2b0b55c0b1d35
bharah08 <111727649+bharah08@users.noreply.github.com>
2023-07-07 10:34:48 +0530

    Update filenamefind.sh

--- filenamefind.sh
+++ filenamefind.sh
@@ -7,3 +7,16 @@ echo "filename is existed"
 else
 echo "filename is not existed"
 fi
+------------------------
+if file not there :--
+
+#!/bin/bash
+echo "find the particler file"
+read filename
+if [ -f $filename ]
+then
+echo "filename is existed"
+else
+echo "filename is not existed creating new file"
+touch $filename
+fi
